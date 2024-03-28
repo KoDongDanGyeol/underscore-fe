824c0f60ad523197974155905f3a62903c068ad3
kkuyu <devils67@naver.com>
2024-03-28 18:25:03 +0900

    Core!: Update Linter
    Update Files
    - .eslintignore
    - .eslintrc.js
    - .prettierignore
    - .prettierrc.js
    Update Package
    - eslint
    - prettier
    - eslint-config-prettier
    - eslint-plugin-next
    - eslint-plugin-prettier
    - eslint-plugin-react
    - eslint-plugin-react-hooks
    - @typescript-eslint/eslint-plugin
    - @typescript-eslint/parser
    
    Issues: #19

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+const prettierConfig = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  trailingComma: "all",
+  endOfLine: "lf",
+  singleQuote: false,
+  arrowParens: "always",
+};
+
+module.exports = prettierConfig;
