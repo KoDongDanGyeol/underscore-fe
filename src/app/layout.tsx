6e4dd2e7cfb71cee91b530ee7c6c5b091b0b55a7
kkuyu <devils67@naver.com>
2024-04-02 18:35:06 +0900

    Core!: Update Package Styled Component (#24)
    Update Packages
    - styled-components
    - @types/styled-components
    Update Files
    - next.config.js
    Update Pages
    - page.tsx
    Update Components
    - config/StyledProvider
    Update Styles
    - global.ts
    - font/notoSansKr.ts
    - theme/color.ts
    - theme/index.ts
    - theme/screen.ts
    - theme/typo.ts
    Update Types
    - styled.d.ts
    
    Issues: #23

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,21 @@
+import type { Metadata } from "next"
+import { PublicEnvScript } from "next-runtime-env"
 import RecoilProvider from "@/components/config/RecoilProvider"
+import StyledProvider from "@/components/config/StyledProvider"
+import notoSansKr from "@/styles/font/notoSansKr"
 import "@/styles/reset.css"
+
+export const metadata: Metadata = {
+  title: {
+    default: "Underscore",
+    template: "%s | Underscore",
+  },
+  description: "Underscore",
+  keywords: "Underscore",
+  icons: {
+    icon: "/favicon.ico",
+  },
+}
 
 interface RootLayoutProps extends React.PropsWithChildren {
   //
@@ -9,9 +25,16 @@ const RootLayout = (props: RootLayoutProps) => {
   const { children } = props
 
   return (
-    <html lang="ko" suppressHydrationWarning>
+    <html lang="ko" className={notoSansKr.variable} suppressHydrationWarning>
+      <head>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <PublicEnvScript />
+      </head>
       <body>
-        <RecoilProvider flag={false}>{children}</RecoilProvider>
+        <RecoilProvider flag={false}>
+          <StyledProvider>{children}</StyledProvider>
+        </RecoilProvider>
       </body>
     </html>
   )
